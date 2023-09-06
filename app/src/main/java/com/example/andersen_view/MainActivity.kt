86acd994a5cd962234fc23d808e9b0e512cc3f42
olgaportnova <yep4yep@gmail.com>
2023-09-06 10:35:50 +0400

    image

--- app/src/main/java/com/example/andersen_view/MainActivity.kt
+++ app/src/main/java/com/example/andersen_view/MainActivity.kt
@@ -1,11 +1,65 @@
 package com.example.andersen_view
 
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.example.andersen_view.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.showImage.setOnClickListener {
+            var imageUrl = binding.editTextImageUrl.text.toString().trim()
+            loadImage(imageUrl)
+        }
     }
+
+
+    private fun loadImage(imageUrl: String) {
+        if (imageUrl.isNotEmpty()) {
+            Glide.with(this)
+                .load(imageUrl)
+                .placeholder(R.drawable.error_image)
+                .listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        Toast.makeText(this@MainActivity, R.string.error_loading, Toast.LENGTH_SHORT).show()
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable?,
+                        model: Any?,
+                        target: com.bumptech.glide.request.target.Target<Drawable>?,
+                        dataSource: DataSource?,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        return false
+                    }
+                })
+                .into(binding.imageView)
+        } else {
+            Toast.makeText(this@MainActivity, R.string.edit_text_empty, Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
+
+
